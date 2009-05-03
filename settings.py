2b86c42fe1ae406fec57e4cc3bc9ce52b475d125
Ian Lewis <IanMLewis@gmail.com>
2009-05-03 22:39:42 +0900

    Added humanize application

--- settings.py
+++ settings.py
@@ -82,6 +82,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.humanize',
     'lifestream',
     'blog',
     'tagging',
