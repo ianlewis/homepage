4fd89eedbc996893124902fdc90090219fefe176
Ian Lewis <IanMLewis@gmail.com>
2009-05-17 12:30:48 +0900

    Changed the blog to use django-pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('blog.views',
     url(r'^(?P<locale>\w{2})/?$', 'blog_page', name="blog_page"),
-    url(r'^(?P<locale>\w{2})/page/(?P<page>\d)/?', 'blog_page', name='blog_page_paged'),
     url(r'^(?P<locale>\w{2})/[^/]+/?', 'blog_detail', name='blog_detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,18 @@
 # Create your views here.
-from django.views.generic import list_detail
+from django.views.generic.list_detail import object_list,object_detail
 
 from models import *
 
-def blog_page(request, locale="en", page=1):
+def blog_page(request, locale="en"):
     defaults = {
         "queryset": Post.objects.active().filter(locale=locale),
-        "paginate_by": 10,
-        "page": page,
     }
-    return list_detail.object_list(request, **defaults)
+    return object_list(request, **defaults)
 
 def blog_detail(request, slug, locale="en"):
     defaults = {
         "queryset": Post.objects.active().filter(locale=locale),
         "slug": slug,
     }
-    return list_detail.object_detail(request, **defaults)
+    return object_detail(request, **defaults)
 
