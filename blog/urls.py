4f2cc2fdfdb7702f2f0a3023765dd5c14560e0e8
Ian Lewis <IanMLewis@gmail.com>
2009-05-17 13:02:26 +0900

    Added a list by tag page to the blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,14 @@
 
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('blog.views',
+urlpatterns = patterns('django.views.generic.simple',
+    # Old tag url redirect
+    url(r'^(?P<locale>\w{2})/(?P<tag>[^/]+);$', 'redirect_to', {'url': '/%(locale)s/tag/%(tag)s'}, name='old_blog_tag_page'),
+)
+
+urlpatterns += patterns('blog.views',
+    url(r'^(?P<locale>\w{2})/tag/(?P<tag>.+)$', 'tag_page', name='blog_tag_page'),
+        
+    url(r'^(?P<locale>\w{2})/(?P<slug>[^/]+)/?$', 'blog_detail', name='blog_detail'),
     url(r'^(?P<locale>\w{2})/?$', 'blog_page', name="blog_page"),
-    url(r'^(?P<locale>\w{2})/[^/]+/?', 'blog_detail', name='blog_detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
 # Create your views here.
 from django.views.generic.list_detail import object_list,object_detail
+from django.http import Http404
 
+from lifestream.util.decorators import allow_methods
 from models import *
 
+@allow_methods('GET')
 def blog_page(request, locale="en"):
-    defaults = {
-        "queryset": Post.objects.active().filter(locale=locale),
-    }
-    return object_list(request, **defaults)
+    return object_list(request, Post.objects.active().filter(locale=locale))
 
+@allow_methods('GET')
 def blog_detail(request, slug, locale="en"):
     defaults = {
         "queryset": Post.objects.active().filter(locale=locale),
@@ -16,3 +17,15 @@ def blog_detail(request, slug, locale="en"):
     }
     return object_detail(request, **defaults)
 
+@allow_methods('GET')
+def tag_page(request, tag, locale="en"):
+    from tagging.utils import get_tag
+    from tagging.models import TaggedItem
+    #import pdb;pdb.set_trace()
+    tag_instance = get_tag(tag)
+    if not tag_instance:
+        raise Http404
+
+    queryset = TaggedItem.objects.get_by_model(Post.objects.active(), tag_instance)
+
+    return object_list(request, queryset)
