fcf91a3993d44ec982a09a88bdc48f93ad7e5732
Ian Lewis <IanMLewis@gmail.com>
2009-05-17 12:30:28 +0900

    Lifestream views didn't need their own object_list method

--- lifestream/views.py
+++ lifestream/views.py
@@ -7,20 +7,13 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, Context, loader
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
-from django.views.generic.list_detail import object_detail
+from django.views.generic.list_detail import object_list,object_detail
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
 from lifestream.util.decorators import allow_methods
 
 from lifestream.models import *
-
-def object_list(request, queryset, template_name, extra_context={}):
-    context = {
-      "object_list": queryset,
-    }
-    context.update(extra_context)
-    return direct_to_template(request, template_name, context)
 
 @allow_methods('GET')
 def main_page(request):
@@ -36,10 +29,14 @@ def main_page(request):
   except Post.DoesNotExist:
       jp_post = None
 
-  return object_list(request, Item.objects.published(), "lifestream/main.html", {
-      "jp_post": jp_post,
-      "en_post": en_post,
-  })
+  return object_list(request, 
+      queryset = Item.objects.published(), 
+      template_name = "lifestream/main.html",
+      extra_context = {
+          "jp_post": jp_post,
+          "en_post": en_post,
+      }
+  )
 
 @allow_methods('GET')
 def tag_page(request, tag):
