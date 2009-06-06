682bf49afde50c2ff354ef3aee6da5f6b5a02841
Ian Lewis <IanMLewis@gmail.com>
2009-06-06 15:56:04 +0900

    Added blog feeds

--- blog/urls.py
+++ blog/urls.py
@@ -2,14 +2,26 @@
 
 from django.conf.urls.defaults import *
 
+from feeds import *
+
 urlpatterns = patterns('django.views.generic.simple',
     # Old tag url redirect
     url(r'^(?P<locale>\w{2})/(?P<tag>[^/]+);$', 'redirect_to', {'url': '/%(locale)s/tag/%(tag)s'}, name='old_blog_tag_page'),
 )
+
+feeds = {
+    'enfeed': LatestEnglishBlogEntries,
+    'jpfeed': LatestJapaneseBlogEntries,
+}
 
 urlpatterns += patterns('blog.views',
     url(r'^(?P<locale>\w{2})/tag/(?P<tag>.+)$', 'tag_page', name='blog_tag_page'),
         
     url(r'^(?P<locale>\w{2})/(?P<slug>[^/]+)/?$', 'blog_detail', name='blog_detail'),
     url(r'^(?P<locale>\w{2})/?$', 'blog_page', name="blog_page"),
+
+)
+
+urlpatterns += patterns('django.contrib.syndication.views',
+    url(r'^feed/(?P<url>.*)$', 'feed', {'feed_dict':feeds}, name='blog_feeds'),
 )
