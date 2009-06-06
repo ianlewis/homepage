b243ff31cd814b36d65e762e336fd38d64caa664
Ian Lewis <IanMLewis@gmail.com>
2009-06-06 15:55:52 +0900

    Moved utility template tags to their own application

--- lifestream/templatetags/lifestream.py
+++ lifestream/templatetags/lifestream.py
@@ -6,7 +6,6 @@
 from datetime import datetime,timedelta
 
 from django.utils.safestring import mark_safe
-from django.utils.translation import ngettext,ugettext_lazy as _
 from django.template import Node, Library, TemplateDoesNotExist, TemplateSyntaxError, Variable
 from django.template.loader import render_to_string
 
@@ -15,52 +14,6 @@ register = Library()
 @register.simple_tag
 def item_class(item):
   return item.feed.domain.replace(".", "-") 
-
-@register.filter
-def friendly_date(date, include_time=False):
-  """
-  Prints a human friendly date.
-  """
-  delta = datetime.utcnow() - date
-   
-  if delta < timedelta(seconds=60):
-    msg = _("Just now")
-  elif delta < timedelta(seconds=60*60):
-    minutes = delta.seconds / 60
-    msg = ngettext('%(minutes)s minute ago',
-            '%(minutes)s minutes ago', minutes) % {
-      'minutes': minutes,
-    }
-  elif delta < timedelta(days=1):
-    hours = int(delta.seconds / 60 / 60)
-    msg = ngettext('%(hours)s hour ago',
-            '%(hours)s hours ago', hours) % {
-      'hours': hours,
-    }
-  elif delta < timedelta(days=7):
-    msg = ngettext('%(days)s day ago',
-            '%(days)s days ago', delta.days) % {
-      'days': delta.days,
-    }
-  elif delta < timedelta(days=31):
-    weeks = int(delta.days / 7)
-    msg = ngettext('%(weeks)s week ago',
-            '%(weeks)s weeks ago', weeks) % {
-      'weeks': weeks,
-    }
-  elif delta < timedelta(days=365):
-    months = int(delta.days / 31)
-    msg = ngettext('%(months)s month ago',
-            '%(months)s months ago', months) % {
-      'months': months,
-    }
-  else:
-    years = int(delta.days / 365)
-    msg = ngettext('%(years)s year ago',
-            '%(years)s years ago', years) % {
-      'years': years,
-    }
-  return mark_safe(msg)
 
 class LifestreamItemNode(Node):
     def __init__(self, item_var):
