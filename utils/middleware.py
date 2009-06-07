a8dc568d8b1afc8e686aeab0a8e226b21ff98127
Ian Lewis <IanMLewis@gmail.com>
2009-06-07 23:58:51 +0900

    Updated naked domain redirect middleware to redirect to the proper url and not the root url every time

--- utils/middleware.py
+++ utils/middleware.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 class WWWRedirectMiddleware(object):
     def process_request(self, request):
-        print request.META['HTTP_HOST']
         if not settings.DEBUG and not request.META['HTTP_HOST'].startswith('www.'):
             current_site = Site.objects.get_current()
-            return HttpResponseRedirect('http://%s' % current_site.domain)
+            return HttpResponseRedirect('http://%s%s' % (current_site.domain, request.path))
