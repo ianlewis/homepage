2eb6f70108204b5f7ddfcb3a409637c7eba3dbb2
Ian Lewis <IanMLewis@gmail.com>
2009-06-13 09:00:25 +0900

    Updated the redirect middleware to be a bit better.

--- utils/middleware.py
+++ utils/middleware.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 class WWWRedirectMiddleware(object):
     def process_request(self, request):
-        if not settings.DEBUG and not request.META['HTTP_HOST'].startswith('www.'):
-            current_site = Site.objects.get_current()
+        current_site = Site.objects.get_current()
+        if not settings.DEBUG and \
+           request.META['HTTP_HOST'] != current_site.domain:
             return HttpResponseRedirect('http://%s%s' % (current_site.domain, request.path))
