f6554fcb0014a0327a184233db3baefd49c472c9
Ian Lewis <IanMLewis@gmail.com>
2009-09-13 13:02:50 +0900

    Added new design for smipple.net

--- static/js/lifestream.js
+++ static/js/lifestream.js
@@ -11,4 +11,13 @@ $(function() {
       $("#"+$(this).attr('rel')).jqmHide();
     });
   }
+
+  //Flickr Images
+  $("div.lifestream-item-inner.flickr-com img").each(function() {
+    var img = $(this);
+    var w = img.width();
+    var extra = -1 * (w - 214)/2;
+    img.css("left", extra+"px");
+    // alert($(this).attr("src")+": "+$(this).width());
+  });
 });
