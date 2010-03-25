5b1c93d3feeb00b65b906d48cc51e636cab20d34
Ian Lewis <IanMLewis@gmail.com>
2010-03-25 17:26:53 +0900

    Upgraded fabfile to the newer API

--- fabfile.py
+++ fabfile.py
@@ -1,63 +1,75 @@
 #:coding=utf8:
-#from fabric.api import run,config
+from fabric.api import *
+from fabric.decorators import runs_once
 
 DEPLOY_USER = 'ianlewis'
 DEPLOY_HOSTS = ['ianlewis.webfactional.com']
 DEPLOY_REV = 'default'
 APP_PATH='~/webapps/homepage'
 
+@runs_once
 def reboot():
-    run("workon homepage;$(app_path)/apache2/bin/restart")
+    run("workon homepage;%(app_path)s/apache2/bin/restart" % env)
 
+@runs_once
 def hg_pull():
-    run("cd $(app_path)/hp;hg pull -r $(rev)")
+    run("cd %(app_path)s/hp;hg pull -r %(rev)s" % env)
 
+@runs_once
 def hg_update():
-    run("cd $(app_path)/hp;hg update -C -r $(rev)")
+    run("cd %(app_path)s/hp;hg update -C -r %(rev)s" % env)
 
+@runs_once
 def install_prereqs():
-    run("workon homepage;cd $(app_path)/hp;pip install -r requirements.txt")
+    run("workon homepage;cd %(app_path)s/hp;pip install -r requirements.txt" % env)
 
 # Needed until South can support reusable apps transparently
+@runs_once
 def run_syncdb():
-    run("workon homepage;cd $(app_path)/hp;python manage.py syncdb")
+    run("workon homepage;cd %(app_path)s/hp;python manage.py syncdb" % env)
 
+@runs_once
 def run_migration():
-    run("workon homepage;cd $(app_path)/hp;python manage.py migrate")
+    run("workon homepage;cd %(app_path)s/hp;python manage.py migrate" % env)
 
+@runs_once
 def compress_css():
-    require("fab_hosts", provided_by=[production])
-    run("workon homepage;rm -f $(app_path)/hp/static/css/all.min.css;for FILE in $(app_path)/hp/static/css/*.css; do csstidy $FILE --template=highest $FILE.tmp; done;for FILE in $(app_path)/hp/static/css/*.tmp; do cat $FILE >> $(app_path)/hp/static/css/all.min.css; done;rm -f $(app_path)/hp/static/css/*.tmp" )
+    require("hosts", provided_by=[production])
+    run("workon homepage;rm -f %(app_path)s/hp/static/css/all.min.css;for FILE in %(app_path)s/hp/static/css/*.css; do csstidy $FILE --template=highest $FILE.tmp; done;for FILE in %(app_path)s/hp/static/css/*.tmp; do cat $FILE >> %(app_path)s/hp/static/css/all.min.css; done;rm -f %(app_path)s/hp/static/css/*.tmp" % env)
 
+@runs_once
 def pull():
-    require('fab_hosts', provided_by=[production])
-    invoke(hg_pull)
-    invoke(hg_update)
+    require('hosts', provided_by=[production])
+    hg_pull()
+    hg_update()
 
+@runs_once
 def update():
-    require("fab_hosts", provided_by=[production])
-    invoke(install_prereqs) 
+    require("hosts", provided_by=[production])
+    install_prereqs() 
 
+@runs_once
 def migrate_db():
-    require("fab_hosts", provided_by=[production])
-    invoke(run_syncdb) 
-    invoke(run_migration)
+    require("hosts", provided_by=[production])
+    run_syncdb() 
+    run_migration()
 
+@runs_once
 def put_settings():
-    require("fab_hosts", provided_by=[production])
+    require("hosts", provided_by=[production])
     put("settings_production.py", "/home/ianlewis/webapps/homepage/hp/settings_local.py")
 
 def deploy():
-    require("fab_hosts", provided_by=[production])
-    invoke(pull)
-    invoke(update)
-    invoke(put_settings)
-    invoke(migrate_db)
-    invoke(compress_css)
-    invoke(reboot)
+    require("hosts", provided_by=[production])
+    pull()
+    update()
+    put_settings()
+    migrate_db()
+    compress_css()
+    reboot()
 
 def production():
-    config.fab_user = DEPLOY_USER
-    config.fab_hosts = DEPLOY_HOSTS
-    config.rev = DEPLOY_REV
-    config.app_path = APP_PATH 
+    env.user = DEPLOY_USER 
+    env.hosts = DEPLOY_HOSTS
+    env.rev = DEPLOY_REV
+    env.app_path = APP_PATH 
