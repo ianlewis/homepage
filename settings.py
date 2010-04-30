b35bcb897ce35055e8f8c0a174a9bb2c03b11cba
Ian Lewis <IanMLewis@gmail.com>
2010-04-30 10:02:15 +0900

    Updated PREPEND_WWW to only be set on the production server

--- settings.py
+++ settings.py
@@ -85,8 +85,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls'
 
-PREPEND_WWW = True
-
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
