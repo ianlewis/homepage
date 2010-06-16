80c35b47a8d78afed8786a0b926b5239600c41db
Ian Lewis <IanMLewis@gmail.com>
2010-06-16 13:21:33 +0900

    more debug

--- homepage/middleware.py
+++ homepage/middleware.py
@@ -75,7 +75,7 @@ class UpdateCacheMiddleware(object):
             cache_key = learn_cache_key(request, response, timeout, self.key_prefix)
             logging.info("%s: learned cache key: %s" % (request.path, cache_key))
             cache.set(cache_key, response, timeout)
-            logging.info("set cache: %s,%s\n\n%s" % (response, timeout, cache.get(cache_key)))
+            logging.info("set cache: %s\n\n%s" % (timeout, cache.get(cache_key)))
         return response
 
 class FetchFromCacheMiddleware(object):
