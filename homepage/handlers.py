612cabdda920e0d88881599e58fea48f6092e592
Ian Lewis <IanMLewis@gmail.com>
2011-02-27 00:47:02 +0900

    Updated handler to include information from the request

--- homepage/handlers.py
+++ homepage/handlers.py
@@ -11,9 +11,14 @@ class DatabaseHandler(logging.Handler):
             source = record.source
         else:
             source = record.name
+
+        if hasattr(record, 'request'):
+            message = "%s\n\n%s" % (record.msg, repr(record.request))
+        else:
+            message = record.msg
         
         try:
-            Log.objects.create(source=source, level=record.levelname, msg=record.msg, host=HOST)
+            Log.objects.create(source=source, level=record.levelname, msg=message, host=HOST)
         except:
             # squelching exceptions sucks, but 500-ing because of a logging error sucks more
             pass
