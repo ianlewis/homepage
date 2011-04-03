dbf1e8ce8a4e681aa848fd4dba90395941375e7f
Ian Lewis <IanMLewis@gmail.com>
2011-04-03 18:43:04 +0900

    Added a traceback to error logging error messages

--- homepage/handlers.py
+++ homepage/handlers.py
@@ -5,6 +5,7 @@ HOST = platform.uname()[1]
 
 class DatabaseHandler(logging.Handler):
     def emit(self, record):
+        import traceback
         from homepage.models import Log
         
         if hasattr(record, 'source'):
@@ -12,11 +13,16 @@ class DatabaseHandler(logging.Handler):
         else:
             source = record.name
 
+        message = record.msg
+
+        if record.exc_info:
+            exc_info = record.exc_info
+            stack_trace = '\n'.join(traceback.format_exception(*record.exc_info))
+            message = "%s\n\n%s" % (message, stack_trace)
+
         if hasattr(record, 'request'):
-            message = "%s\n\n%s" % (record.msg, repr(record.request))
-        else:
-            message = record.msg
-        
+            message = "%s\n\n%s" % (message, repr(record.request))
+         
         try:
             Log.objects.create(source=source, level=record.levelname, msg=message, host=HOST)
         except:
