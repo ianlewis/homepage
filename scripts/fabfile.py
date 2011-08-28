a63828c1cf1dc925637a4a468b43b06e51315068
Ian Lewis <IanMLewis@gmail.com>
2011-08-28 16:19:44 +0900

    Backed out changes for aws

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -2,21 +2,17 @@
 
 from __future__ import with_statement 
 
-import os
-
 from fabric.api import *
 from fabric.decorators import runs_once
 
-ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 def _run_app_cmd(cmd):
     with prefix('source %(venv_path)s/bin/activate' % env):
-        with cd('%(app_path)s/app' % env):
-            sudo(cmd)
+        with cd('%(app_path)s/hp/app' % env):
+            run(cmd)
 
 @runs_once
 def reboot():
-    _run_app_cmd('%(app_path)s/scripts/gunicorn.sh reload' % env)
+    _run_app_cmd('%(app_path)s/apache2/bin/restart' % env)
 
 @runs_once
 def hg_pull():
@@ -28,7 +24,7 @@ def hg_update():
 
 @runs_once
 def install_prereqs():
-    _run_app_cmd('pip install -E %(venv_path)s -r %(app_path)s/requirements.txt' % env)
+    _run_app_cmd('pip install -E %(venv_path)s -r %(app_path)s/hp/requirements.txt' % env)
 
 # Needed until South can support reusable apps transparently
 @runs_once
@@ -38,7 +34,7 @@ def run_syncdb():
 @runs_once
 def collect_static():
     _run_app_cmd("python manage.py collectstatic --noinput --settings=%(settings)s" % env)
-    sudo("mkdir -p %(app_path)s/site_media/media;" % env)
+    run("mkdir -p %(app_path)s/hp/site_media/media;" % env)
 
 @runs_once
 def run_migration():
@@ -47,7 +43,7 @@ def run_migration():
 @runs_once
 def compress_css():
     require("hosts", provided_by=[production])
-    _run_app_cmd("rm -f %(app_path)s/site_media/static/css/all.min.css;for FILE in %(app_path)s/site_media/static/css/*.css; do csstidy $FILE --template=highest $FILE.tmp; done;for FILE in %(app_path)s/site_media/static/css/*.tmp; do cat $FILE >> %(app_path)s/site_media/static/css/all.min.css; done;rm -f %(app_path)s/site_media/static/css/*.tmp" % env)
+    _run_app_cmd("rm -f %(app_path)s/hp/site_media/static/css/all.min.css;for FILE in %(app_path)s/hp/site_media/static/css/*.css; do csstidy $FILE --template=highest $FILE.tmp; done;for FILE in %(app_path)s/hp/site_media/static/css/*.tmp; do cat $FILE >> %(app_path)s/hp/site_media/static/css/all.min.css; done;rm -f %(app_path)s/hp/site_media/static/css/*.tmp" % env)
 
 @runs_once
 def pull():
@@ -69,7 +65,7 @@ def migrate_db():
 @runs_once
 def put_settings():
     require("hosts", provided_by=[production])
-    put("%s/app/settings_production.py" % ROOT_PATH, "%(app_path)s/app/settings_local.py" % env, use_sudo=True)
+    put("../app/settings_production.py", "/home/ianlewis/webapps/homepage/hp/app/settings_local.py")
 
 def deploy():
     require("hosts", provided_by=[production])
@@ -83,9 +79,9 @@ def deploy():
     reboot()
 
 def production():
-    env.user = 'ubuntu'
-    env.hosts = ['ec2-175-41-217-89.ap-northeast-1.compute.amazonaws.com']
-    env.rev = 'aws' 
+    env.user = 'ianlewis'
+    env.hosts = ['ianlewis.webfactional.com']
+    env.rev = 'default' 
     env.settings = 'settings_local'
-    env.app_path = '/var/www/vhosts/homepage'
-    env.venv_path = '/var/www/venvs/homepage'
+    env.app_path = '~/webapps/homepage'
+    env.venv_path = '~/webapps/homepage/venvs/homepage'
