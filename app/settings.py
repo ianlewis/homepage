b9b71dc3e62fcd528b9b8fd85b447ec787987da5
Ian Lewis <IanMLewis@gmail.com>
2011-10-25 21:14:26 +0900

    Updated logging settings

--- app/settings.py
+++ app/settings.py
@@ -170,29 +170,28 @@ DISQUS_WEBSITE_SHORTNAME = ''
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s][%(name)s] %(levelname)s %(message)s',
+            'datefmt': "%Y-%m-%d %H:%M:%S",
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
         'null': {
             'level':'DEBUG',
             'class':'django.utils.log.NullHandler',
         },
-        'database': {
-            'level': 'DEBUG',
-            'class': 'homepage.handlers.DatabaseHandler',
-        }
     },
     'loggers': {
-        '': {
-            'handlers':['database'],
-            'propagate': True,
-            'level':'WARNING', 
-        },
-        'django': { 
-            'handlers':['null'],
-            'propagate': False,
-            'level':'INFO', 
-        },
         'django.request': {
-            'handlers': ['database'],
+            'handlers': ['mail_admins'],
             'propagate': False,
             'level':'WARNING',
         },
