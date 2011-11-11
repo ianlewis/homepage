523a5aa4ff376b4732bd1a166addc94647cebce6
Ian Lewis <IanMLewis@gmail.com>
2011-11-11 22:48:28 +0900

    Fixed dumb mistake

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -1,79 +1,111 @@
 #:coding=utf8:
 
-from __future__ import with_statement 
-
 import os
 
-from fabric.api import *
-from fabric.decorators import runs_once
+from fabric.api import cd, sudo, env, put
+from fabric.decorators import roles
+from fabric.context_managers import prefix
 
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def _annotate_hosts_with_ssh_config_info():
+    """
+    ~/.ssh/config ファイルがあれば、設定を読み込む
+    """
+    from os.path import expanduser
+    from paramiko.config import SSHConfig
+
+    def hostinfo(host, config):
+        hive = config.lookup(host)
+        if 'hostname' in hive:
+            host = hive['hostname']
+        if 'user' in hive:
+            host = '%s@%s' % (hive['user'], host)
+        if 'port' in hive:
+            host = '%s:%s' % (host, hive['port'])
+        return host
+
+    try:
+        config_file = file(expanduser('~/.ssh/config'))
+    except IOError:
+        pass
+    else:
+        config = SSHConfig()
+        config.parse(config_file)
+        keys = [config.lookup(host).get('identityfile', None)
+            for host in env.hosts]
+        env.key_filename = [expanduser(key) for key in keys if key is not None]
+        env.hosts = [hostinfo(host, config) for host in env.hosts]
+
+        if env.roledefs:
+            for roledef, hosts in env.roledefs.items():
+                env.roledefs[roledef] = [hostinfo(host, config) for host in hosts]
+                keys = [config.lookup(host).get('identityfile', None) for host in hosts]
+                env.key_filename.extend([expanduser(key) for key in keys if key is not None])
 
 def _run_app_cmd(cmd):
     with prefix('source %(venv_path)s/bin/activate' % env):
         with cd('%(app_path)s/app' % env):
-            sudo(cmd)
+            sudo(cmd, user=env.deploy_user)
 
-@runs_once
+@roles('webservers')
 def reboot():
     # Need to wait for gunicorn to daemonize
-    _run_app_cmd('%(app_path)s/scripts/gunicorn.sh reload && sleep 5' % env)
+    sudo('svc -t %(service_path)s' % env)
 
-@runs_once
+@roles('webservers')
 def hg_pull():
-    _run_app_cmd('hg pull -r %(rev)s' % env)
+    with cd(env.app_path):
+        sudo('hg pull -r %(rev)s' % env, user=env.deploy_user)
 
-@runs_once
+@roles('webservers')
 def hg_update():
-    _run_app_cmd('hg update -C -r %(rev)s' % env)
+    with cd(env.app_path):
+        sudo('hg update -C -r %(rev)s' % env, user=env.deploy_user)
 
-@runs_once
+@roles('webservers')
 def install_prereqs():
-    _run_app_cmd('pip install -E %(venv_path)s -r %(app_path)s/requirements.txt' % env)
+    sudo('pip install -E %(venv_path)s -r %(app_path)s/requirements.txt' % env, user=env.deploy_user)
 
 # Needed until South can support reusable apps transparently
-@runs_once
+@roles('webservers')
 def run_syncdb():
     _run_app_cmd("python manage.py syncdb --settings=%(settings)s" % env)
 
-@runs_once
+@roles('webservers')
 def collect_static():
     _run_app_cmd("python manage.py collectstatic --noinput --settings=%(settings)s" % env)
     sudo("mkdir -p %(app_path)s/site_media/media;" % env)
 
-@runs_once
+@roles('webservers')
 def run_migration():
     _run_app_cmd("python manage.py migrate --settings=%(settings)s" % env)
 
-@runs_once
+@roles('webservers')
 def compress_css():
-    require("hosts", provided_by=[production])
-    _run_app_cmd("rm -f %(app_path)s/site_media/static/css/all.min.css;for FILE in %(app_path)s/site_media/static/css/*.css; do csstidy $FILE --template=highest $FILE.tmp; done;for FILE in %(app_path)s/site_media/static/css/*.tmp; do cat $FILE >> %(app_path)s/site_media/static/css/all.min.css; done;rm -f %(app_path)s/site_media/static/css/*.tmp" % env)
+    sudo("rm -f %(app_path)s/site_media/static/css/all.min.css;for FILE in %(app_path)s/site_media/static/css/*.css; do csstidy $FILE --template=highest $FILE.tmp; done;for FILE in %(app_path)s/site_media/static/css/*.tmp; do cat $FILE >> %(app_path)s/site_media/static/css/all.min.css; done;rm -f %(app_path)s/site_media/static/css/*.tmp" % env, user=env.deploy_user)
 
-@runs_once
+@roles('webservers')
 def pull():
-    require('hosts', provided_by=[production])
     hg_pull()
     hg_update()
 
-@runs_once
+@roles('webservers')
 def update():
-    require("hosts", provided_by=[production])
     install_prereqs() 
 
-@runs_once
+@roles('webservers')
 def migrate_db():
-    require("hosts", provided_by=[production])
     #run_syncdb() 
     run_migration()
 
-@runs_once
+@roles('webservers')
 def put_settings():
-    require("hosts", provided_by=[production])
     put("%s/app/settings_production.py" % ROOT_PATH, "%(app_path)s/app/settings_local.py" % env, use_sudo=True)
+    sudo('chown %(deploy_user)s:%(deploy_user)s "%(app_path)s/app/settings_local.py"' % env)
 
+@roles('webservers')
 def deploy():
-    require("hosts", provided_by=[production])
     pull()
     update()
     put_settings()
@@ -84,9 +116,14 @@ def deploy():
     reboot()
 
 def production():
-    env.user = 'ubuntu'
-    env.hosts = ['www.ianlewis.org']
+    env.deploy_user = 'www-data'
+    env.roledefs.update({
+        'webservers': ['www.ianlewis.org'],
+    })
     env.rev = 'default' 
     env.settings = 'settings_local'
     env.app_path = '/var/www/vhosts/homepage'
     env.venv_path = '/var/www/venvs/homepage'
+    env.service_path = '/etc/service/homepage'
+
+    _annotate_hosts_with_ssh_config_info()
