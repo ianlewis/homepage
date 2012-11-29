4fc477e3107566f94ccfd8b3c3731a6a41e18bf7
Ian Lewis <IanMLewis@gmail.com>
2012-11-30 02:19:31 +0900

    Updated wsgi app to be more Django 1.4 friendly.

--- scripts/production_wsgi.py
+++ scripts/production_wsgi.py
@@ -1,18 +1,6 @@
 import os
-import sys
-import site
 
-VHOST_PATH = '/var/www/vhosts/homepage'
-VENV_PATH = '/var/www/venvs/homepage'
-PYTHON_PATH = os.path.join(VENV_PATH, 'lib', 'python2.5')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homepage.settings_local")
 
-site.addsitedir(os.path.join(PYTHON_PATH, 'site-packages'))
-# Don't load from this path since it has an old version of django there
-#site.addsitedir(os.path.join(PYTHON_PATH, 'lib', 'python2.5', 'site-packages'))
-
-sys.path.insert(0, os.path.join(VHOST_PATH, 'app'))
-
-from django.core.handlers.wsgi import WSGIHandler
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_local'
-application = WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
