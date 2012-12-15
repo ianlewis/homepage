d7e126111abed2a301e0c7bd92964cf72cc4c9ae
Ian Lewis <IanMLewis@gmail.com>
2012-12-15 23:16:45 +0900

    Only allow "en" or "jp" for the locale in the url.

--- homepage/blog/urls.py
+++ homepage/blog/urls.py
@@ -9,16 +9,16 @@ from homepage.blog.feeds import (
 
 urlpatterns = patterns('django.views.generic.simple',
     # Old tag url redirect
-    url(r'^(?P<locale>\w{2})/(?P<tag>[^/]+);$', 'redirect_to', {'url': '/%(locale)s/tag/%(tag)s'}, name='old_blog_tag_page'),
+    url(r'^(?P<locale>jp|en)/(?P<tag>[^/]+);$', 'redirect_to', {'url': '/%(locale)s/tag/%(tag)s'}, name='old_blog_tag_page'),
 )
 
 urlpatterns += patterns('homepage.blog.views',
     url(r'^admin/blog/post/(?P<object_id>[0-9]+)/preview$', 'blog_detail_preview', name='blog_detail_preview'),
 
-    url(r'^(?P<locale>\w{2})/tag/(?P<tag>.+)$', 'tag_page', name='blog_tag_page'),
+    url(r'^(?P<locale>jp|en)/tag/(?P<tag>.+)$', 'tag_page', name='blog_tag_page'),
         
-    url(r'^(?P<locale>\w{2})/(?P<slug>[^/]+)/?$', 'blog_detail', name='blog_detail'),
-    url(r'^(?P<locale>\w{2})/?$', 'blog_page', name="blog_page"),
+    url(r'^(?P<locale>jp|en)/(?P<slug>[^/]+)/?$', 'blog_detail', name='blog_detail'),
+    url(r'^(?P<locale>jp|en)/?$', 'blog_page', name="blog_page"),
 )
 
 urlpatterns += patterns('',
