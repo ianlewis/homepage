093eceaa4121be189d9618fe208078e6d21698c9
Ian Lewis <IanMLewis@gmail.com>
2015-05-19 01:10:41 +0900

    version up

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ install_requires = [
 
 setup(
     name="homepage",
-    version="0.0.15",
+    version="0.0.16",
     author="Ian Lewis",
     author_email="ianmlewis@gmail.com",
     description="Ian Lewis' homepage at www.ianlewis.org",
