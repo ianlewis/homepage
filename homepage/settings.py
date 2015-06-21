c2bf00562f7abbc1b308fb9dd7857c615412f6d4
Ian Lewis <IanMLewis@gmail.com>
2015-06-21 09:14:51 +0900

    Fixed typo bug

--- homepage/settings.py
+++ homepage/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
-    "django.core.context_processors.rewhere command bashquest",
+    "django.core.context_processors.request",
     "homepage.core.context_processors.debug",
 )
 
