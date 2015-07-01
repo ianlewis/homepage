cbf35199f033542d3f1ab30e346f02d5aa6f5313
Ian Lewis <IanMLewis@gmail.com>
2015-07-01 22:23:08 +0900

    Removed lead_image

--- homepage/blog/models.py
+++ homepage/blog/models.py
@@ -1,15 +1,11 @@
 #:coding=utf8:
 
-import uuid
-import os
-
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from tagging.fields import TagField
-from sorl.thumbnail import ImageField
 
 from datetime import datetime
 
@@ -18,32 +14,31 @@ BLOG_LOCALES = (
     ('en', u'English'),
 )
 
+
 class PostManager(models.Manager):
     def published(self):
-        return self.filter(pub_date__lt = datetime.now(), active=True)
+        return self.filter(pub_date__lt=datetime.now(), active=True)
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, verbose_name=u"author")
     slug = models.SlugField(u"slug", max_length=50, unique=True, db_index=True)
     title = models.TextField(u"title")
-    lead = models.TextField(u"lead", blank=True, null=True, default=None, max_length=600)
-    lead_image = ImageField(verbose_name="lead image", null=True, blank=True,
-        upload_to=lambda instance, filename: 'thumbs/%s%s' % (
-            uuid.uuid4().hex,
-            os.path.splitext(filename)[1].lower(),
-        ),
-    )
+    lead = models.TextField(u"lead", blank=True, null=True, default=None,
+                            max_length=600)
     content = models.TextField(u"content")
     markup_type = models.CharField(max_length=10, choices=(
         ("md", "Markdown"),
         ("rst", "reStructuredText"),
         ("html", "HTML"),
     ), default="md")
-    locale = models.CharField(u'locale', max_length=20, choices=BLOG_LOCALES, default="en", db_index=True)
+    locale = models.CharField(u'locale', max_length=20, choices=BLOG_LOCALES,
+                              default="en", db_index=True)
     tags = TagField()
 
     active = models.BooleanField(u'published', default=False, db_index=True)
-    pub_date = models.DateTimeField(u'published', default=datetime.now, db_index=True)
+    pub_date = models.DateTimeField(u'published', default=datetime.now,
+                                    db_index=True)
     create_date = models.DateTimeField(u'created', default=datetime.now)
 
     objects = PostManager()
@@ -61,9 +56,10 @@ class Post(models.Model):
             "en": "en",
             "jp": "ja",
         }.get(self.locale, "en")
-    
+
     def get_full_url(self):
-        return 'http://%s%s' % (Site.objects.get_current().domain, self.get_absolute_url())
+        return 'http://%s%s' % (Site.objects.get_current().domain,
+                                self.get_absolute_url())
 
     def __unicode__(self):
         return self.title
