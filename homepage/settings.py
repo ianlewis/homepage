32b31451548504bb48c59257e4d05717e2d051ce
Ian Lewis <IanMLewis@gmail.com>
2015-07-01 21:16:48 +0900

    Removed unused Django comments app

--- homepage/settings.py
+++ homepage/settings.py
@@ -180,7 +180,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.comments',
     'django.contrib.flatpages',
     'django.contrib.staticfiles',
 
