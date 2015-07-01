245e10b9881b639023eecfd9ec921f7d70ad1b5a
Ian Lewis <IanMLewis@gmail.com>
2015-07-01 22:46:11 +0900

    Fixed pagination window

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ install_requires = [
 
 setup(
     name="homepage",
-    version="0.0.20",
+    version="0.0.21",
     author="Ian Lewis",
     author_email="ianmlewis@gmail.com",
     description="Ian Lewis' homepage at www.ianlewis.org",
