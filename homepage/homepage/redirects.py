b19a7425b20fd45008e7b2be9f935203a06bc1d1
Ian Lewis <ianmlewis@gmail.com>
2016-10-02 18:30:25 +0900

    Updated an old lifestream URL redirect

--- homepage/homepage/redirects.py
+++ homepage/homepage/redirects.py
@@ -27,13 +27,14 @@ def redirect_to(request, url, permanent=True, **kwargs):
 
 urlpatterns = patterns(
     '',
-    url(r'^page/(?P<page>\d+)/?$', redirect_to, {'url': '/?page=%(page)s'}, name="lifestream_page_redirect"),
+
     url(r'^projects(?:/.*)?$', redirect_to, {'url': '/'}, name="projects_redirect"),
+
     #url(r'^en/about(?:/.*)?$', redirect_to, {'url': '/'}, name="en_about_redirect"),
     url(r'^jp/about(?:/.*)?$', redirect_to, {'url': '/en/about/'}, name="jp_about_redirect"),
     url(r'^jp/aboutjp$', redirect_to, {'url': '/en/about/'}, name="jp_about_redirect2"),
 
-    # Legacy urls for the blog
+    # Really old legacy urls from the b2evo blog
     url(r'^index.php/(?P<locale>\w{2})/?$', redirect_to, {'url': '/%(locale)s/'}),
     url(r'^index.php$', redirect_to, {'url': '/en/'}),
     url(r'^index.php/(?P<locale>\w{2})/(?P<slug>[^/]+)/?$', redirect_to, {'url': '/%(locale)s/%(slug)s'}),
@@ -43,4 +44,5 @@ urlpatterns = patterns(
 
     # Old lifestream urls.
     url(r'^items/', redirect_to, {'url': '/'}),
+    url(r'^page/(?P<page>\d+)/?$', redirect_to, {'url': '/'}, name="lifestream_page_redirect"),
 )
