0eb6d8609ebb8271467f4830ba1907bb2cd2fc1a
Ian Lewis <ianlewis@google.com>
2017-09-01 10:49:51 +0900

    Removed dyndns stuff

--- dyndns/create-certs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-openssl genrsa -out ca-key.pem 2048
-# openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=dyndns.ianlewis.org"
-openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=localhost"
-
-openssl genrsa -out client-key.pem 2048
-openssl req -new -key client-key.pem -out client.csr -subj "/CN=dyndns-client"
-openssl x509 -req -in client.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out client.pem -days 365
