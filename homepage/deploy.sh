fccfdd2cde47363eac24383c28d3414de05c7356
Ian Lewis <ianmlewis@gmail.com>
2018-03-24 17:38:15 +0900

    Updated cloudbuild to deploy to staging

--- homepage/deploy.sh
+++ homepage/deploy.sh
@@ -1,8 +1,16 @@
-if [ "$1" = "" ]; then
-    echo "Usage: $0 NAMESPACE"
-    exit 1
-fi 
-NAMESPACE=$1
-export HOMEPAGE_VERSION=$(git rev-parse --short master)
+#!/bin/bash
+
+HOMEPAGE_VERSION=$SHORT_SHA
+if [ "$HOMEPAGE_VERSION" = "" ]; then
+    HOMEPAGE_VERSION=$(git rev-parse --short master)
+fi
+export HOMEPAGE_VERSION
+
+PROJECT=$PROJECT_ID
+if [ "$PROJECT" = "" ]; then
+    PROJECT=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
+fi
+export PROJECT
+
 export NGINX_VERSION=1.13.10
-envsubst < deploy.yaml | kubectl apply -n ${NAMESPACE} -f -
+envsubst < deploy.yaml.tmpl
