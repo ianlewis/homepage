126ceaa750eef0c23eb0b0422bdcbc9ddf570d8e
Ian Lewis <ianmlewis@gmail.com>
2018-09-24 20:10:36 +0900

    Moved backup to separate repo

--- backup/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mysqldump --host=mysql --user=`cat /etc/secrets/db-user` --password=`cat /etc/secrets/db-password` --single-transaction homepage \
-    | bzip2 \
-    | /opt/gsutil/gsutil cp - gs://${BUCKET}/${NAMESPACE}.`date +%Y%m%d%H%M%S`.sql.bz2
