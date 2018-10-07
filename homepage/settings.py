8e67d00b73c27f6dd49b71b6cb267a378282c2fd
Ian Lewis <ianmlewis@gmail.com>
2018-10-07 17:00:01 +0900

    Added template debug panel to debug toolbar

--- homepage/settings.py
+++ homepage/settings.py
@@ -188,7 +188,7 @@ INSTALLED_APPS = [
     'homepage.blog',
 ]
 if TESTING:
-    INSTALLED_APPS += ['debug_toolbar']
+    INSTALLED_APPS += ['debug_toolbar', 'template_profiler_panel']
 
 CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
 CONSTANCE_CONFIG = {
@@ -271,6 +271,22 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 def show_toolbar(request):
     return TESTING
+
+DEBUG_TOOLBAR_PANELS = [
+	'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+	'template_profiler_panel.panels.template.TemplateProfilerPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
 
 # Allow any subdomain of ianlewis.org
 _allowed_hosts = ['.ianlewis.org', '.ianlewis.org.']
