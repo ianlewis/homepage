0717d243d10b74d7367d465e4fe4f81ab307619e
Ian Lewis <ianmlewis@gmail.com>
2019-08-05 17:35:04 +0900

    Update library versions

--- homepage/blog/templatetags/blog_tags.py
+++ homepage/blog/templatetags/blog_tags.py
@@ -1,4 +1,4 @@
-#:coding=utf-8:
+# :coding=utf-8:
 
 import re
 import logging
@@ -19,12 +19,8 @@ from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import get_lexer_by_name, TextLexer
 
-from homepage.core.templatetags.utility_tags import (
-    rst_to_html,
-    md_to_html,
-) 
+from homepage.core.templatetags.utility_tags import rst_to_html, md_to_html
 from homepage.blog.pb import blog_pb2
-
 
 
 register = Library()
@@ -38,32 +34,52 @@ def flag(argument):
     else:
         return True
 
-def pygments_directive(name, arguments, options, content, lineno,
-                       content_offset, block_text, state, state_machine):
+
+def pygments_directive(
+    name,
+    arguments,
+    options,
+    content,
+    lineno,
+    content_offset,
+    block_text,
+    state,
+    state_machine,
+):
     try:
         lexer = get_lexer_by_name(arguments[0], **options)
     except (ValueError, IndexError):
         # no lexer found - use the text one instead of an exception
         lexer = TextLexer()
-    parsed = highlight(u"\n".join(content), lexer,
-                       HtmlFormatter(cssclass="highlight notranslate"))
+    parsed = highlight(
+        u"\n".join(content), lexer, HtmlFormatter(cssclass="highlight notranslate")
+    )
     return [nodes.raw("", parsed, format="html")]
+
+
 pygments_directive.arguments = (0, 1, False)
 pygments_directive.content = 1
-pygments_directive.options = OPTIONS = {
-    'startinline': flag,  # For PHP
-}
+pygments_directive.options = OPTIONS = {"startinline": flag}  # For PHP
 
 directives.register_directive("sourcecode", pygments_directive)
 directives.register_directive("code-block", pygments_directive)
 
-align_h_values = ('left', 'center', 'right')
-align_v_values = ('top', 'middle', 'bottom')
+align_h_values = ("left", "center", "right")
+align_v_values = ("top", "middle", "bottom")
 align_values = align_v_values + align_h_values
 
 
-def lightbox_directive(name, arguments, options, content, lineno,
-                       content_offset, block_text, state, state_machine):
+def lightbox_directive(
+    name,
+    arguments,
+    options,
+    content,
+    lineno,
+    content_offset,
+    block_text,
+    state,
+    state_machine,
+):
     """
     lightbox image directive
 
@@ -74,52 +90,58 @@ def lightbox_directive(name, arguments, options, content, lineno,
         Title
     """
     classes = ["lightbox-img"]
-    if 'align' in options:
-        directives.choice(options['align'], align_values)
+    if "align" in options:
+        directives.choice(options["align"], align_values)
 
         if isinstance(state, states.SubstitutionDef):
             # Check for align_v_values.
-            if options['align'] not in align_v_values:
-                raise DirectiveError(3,
+            if options["align"] not in align_v_values:
+                raise DirectiveError(
+                    3,
                     'Error in "%s" directive: "%s" is not a valid value '
                     'for the "align" option within a substitution '
                     'definition.  Valid values for "align" are: "%s".'
-                    % (name, options['align'],
-                       '", "'.join(align_v_values)))
-        elif options['align'] not in align_h_values:
-            raise DirectiveError(3,
+                    % (name, options["align"], '", "'.join(align_v_values)),
+                )
+        elif options["align"] not in align_h_values:
+            raise DirectiveError(
+                3,
                 'Error in "%s" directive: "%s" is not a valid value for '
                 'the "align" option.  Valid values for "align" are: "%s".'
-                % (name, options['align'],
-                   '", "'.join(align_h_values)))
+                % (name, options["align"], '", "'.join(align_h_values)),
+            )
 
-        classes.append('align-%s' % options['align'])
+        classes.append("align-%s" % options["align"])
 
     thumb_href = arguments[0]
     img_href = arguments[1]
     title = u" ".join(content)
-    alt = options.get('alt', '')
+    alt = options.get("alt", "")
 
-    html = '<a title="%(title)s" rel="lightbox" href="%(img_href)s"><img %(classes)s src="%(thumb_href)s" title="%(title)s" alt="%(alt)s"/></a>' % {
-        "classes": 'class="%s"' % " ".join(classes) if classes else "",
-        "title": title,
-        "img_href": img_href,
-        "thumb_href": thumb_href,
-        "alt": alt,
-    }
+    html = (
+        '<a title="%(title)s" rel="lightbox" href="%(img_href)s"><img %(classes)s src="%(thumb_href)s" title="%(title)s" alt="%(alt)s"/></a>'
+        % {
+            "classes": 'class="%s"' % " ".join(classes) if classes else "",
+            "title": title,
+            "img_href": img_href,
+            "thumb_href": thumb_href,
+            "alt": alt,
+        }
+    )
     return [nodes.raw("", html, format="html")]
+
+
 lightbox_directive.arguments = (2, 0, False)
 lightbox_directive.content = 1
 lightbox_directive.options = {
-    'alt': directives.unchanged,
-    'height': directives.length_or_unitless,
-    'width': directives.length_or_percentage_or_unitless,
-    'scale': directives.percentage,
-
-    'align': lambda argument: directives.choice(argument, align_values),
-    'name': directives.unchanged,
-    'target': directives.unchanged_required,
-    'class': directives.class_option,
+    "alt": directives.unchanged,
+    "height": directives.length_or_unitless,
+    "width": directives.length_or_percentage_or_unitless,
+    "scale": directives.percentage,
+    "align": lambda argument: directives.choice(argument, align_values),
+    "name": directives.unchanged,
+    "target": directives.unchanged_required,
+    "class": directives.class_option,
 }
 
 
@@ -135,6 +157,8 @@ def to_html(obj):
         html = mark_safe(obj.content)
 
     return html
+
+
 register.filter("to_html", to_html)
 
 
@@ -145,18 +169,19 @@ def show_post_brief(context, post):
         "can_edit": context["user"].is_staff,
     }
 
-register.inclusion_tag("blog/post_brief.html",
-                       takes_context=True)(show_post_brief)
+
+register.inclusion_tag("blog/post_brief.html", takes_context=True)(show_post_brief)
 
 
 @register.filter
 def date_microformat(d):
-    '''
+    """
         Microformat version of a date.
         2009-02-10T02:58:00+00:00 (ideal)
         2009-02-09T17:54:41.181868-08:00 (mine)
-    '''
+    """
     return d.isoformat()
+
 
 STRIKE_REPL_RE = re.compile("< *strike *>[^<]*</ *strike *>", re.IGNORECASE)
 
@@ -205,17 +230,16 @@ def to_lead(obj, max_len=None):
         try:
             lead = html_to_text(html)
         except HTMLParser.HTMLParseError, e:
-            logger.error('HTML Parse error: "%s" for text "%s"' % (
-                e,
-                html,
-            ))
+            logger.error('HTML Parse error: "%s" for text "%s"' % (e, html))
             return ""
 
     if not max_len:
         max_len = 300 if obj.locale == "jp" else 600
     return abbrev(lead, max_len, "[...]")
 
+
 register.filter("to_lead", to_lead)
+
 
 class AnchorParser(HTMLParser.HTMLParser):
     def __init__(self):
@@ -223,10 +247,11 @@ class AnchorParser(HTMLParser.HTMLParser):
         self.img = None
 
     def handle_starttag(self, tag, attrs):
-        if not self.img and tag == 'img':
+        if not self.img and tag == "img":
             for name, val in attrs:
-                if name == 'src':
+                if name == "src":
                     self.img = val
+
 
 def first_image(html):
     """
@@ -237,12 +262,17 @@ def first_image(html):
     parser.feed(html)
     return parser.img
 
+
 register.filter("first_image", first_image)
 
+
 def timestamp(value, arg=None):
-	"""
-	Renders the given UTC timestamp as a date.
-	"""
-	return datefilter(datetime.utcfromtimestamp(value).replace(tzinfo=timezone.utc), arg)
+    """
+    Renders the given UTC timestamp as a date.
+    """
+    return datefilter(
+        datetime.utcfromtimestamp(value).replace(tzinfo=timezone.utc), arg
+    )
+
 
 register.filter("timestamp", timestamp)
--- homepage/core/templatetags/utility_tags.py
+++ homepage/core/templatetags/utility_tags.py
@@ -1,16 +1,16 @@
-#:coding=utf8:
+# :coding=utf8:
 
 import re
 import markdown
 
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 from docutils.writers import html4css1
 from docutils.core import publish_parts
 from docutils import nodes
 
 from django.utils.safestring import mark_safe
-from django.utils.translation import ngettext,ugettext_lazy as _
+from django.utils.translation import ngettext, ugettext_lazy as _
 from django.template.defaultfilters import stringfilter
 from django import template
 
@@ -20,10 +20,12 @@ register = template.Library()
 
 register.filter(batch)
 
+
 class HTMLWriter(html4css1.Writer):
     def __init__(self):
         html4css1.Writer.__init__(self)
         self.translator_class = HTMLTranslator
+
 
 class HTMLTranslator(html4css1.HTMLTranslator):
     named_tags = []
@@ -33,11 +35,18 @@ class HTMLTranslator(html4css1.HTMLTranslator):
         self.body.append("<code>%s</code>" % node.astext())
         raise nodes.SkipNode
 
+
 def rst_to_html(value):
-    parts = publish_parts(source=value, writer=HTMLWriter(),
-                          settings_overrides={"initial_header_level": 2})
+    parts = publish_parts(
+        source=value,
+        writer=HTMLWriter(),
+        settings_overrides={"initial_header_level": 2},
+    )
     return mark_safe(parts["fragment"])
+
+
 register.filter("rst_to_html", rst_to_html)
+
 
 def md_to_html(value):
     """
@@ -47,19 +56,19 @@ def md_to_html(value):
         markdown.markdown(
             text=value,
             extensions=[
-                'markdown.extensions.fenced_code',
-                'markdown.extensions.codehilite',
-                'markdown.extensions.tables',
-                'markdown.extensions.smart_strong',
+                "markdown.extensions.fenced_code",
+                "markdown.extensions.codehilite",
+                "markdown.extensions.tables",
             ],
             extension_configs={
-                'markdown.extensions.codehilite': {
-                    'css_class': 'highlight',
-                }
-            }
+                "markdown.extensions.codehilite": {"css_class": "highlight"}
+            },
         )
     )
+
+
 register.filter("md_to_html", md_to_html)
+
 
 @register.filter
 @stringfilter
@@ -74,68 +83,75 @@ def truncate_chars(value, max_length):
     except:
         return ""
 
+
 def stripentities(value):
     """Strips all HTML entities"""
     from django.utils.html import strip_entities
+
     return strip_entities(value)
+
+
 stripentities.is_safe = True
 stripentities = stringfilter(stripentities)
 register.filter(stripentities)
 
+
 @register.filter
 def friendly_date(date, include_time=False):
-  """
+    """
   Prints a human friendly date.
   """
-  delta = datetime.now() - date
-   
-  if delta < timedelta(seconds=60):
-    msg = _("Just now")
-  elif delta < timedelta(seconds=60*60):
-    minutes = delta.seconds / 60
-    msg = ngettext('%(minutes)s minute ago',
-            '%(minutes)s min ago', minutes) % {
-      'minutes': minutes,
-    }
-  elif delta < timedelta(days=1):
-    hours = int(delta.seconds / 60 / 60)
-    msg = ngettext('%(hours)s hour ago',
-            '%(hours)s hours ago', hours) % {
-      'hours': hours,
-    }
-  elif delta < timedelta(days=7):
-    msg = ngettext('%(days)s day ago',
-            '%(days)s days ago', delta.days) % {
-      'days': delta.days,
-    }
-  elif delta < timedelta(days=31):
-    weeks = int(delta.days / 7)
-    msg = ngettext('%(weeks)s week ago',
-            '%(weeks)s weeks ago', weeks) % {
-      'weeks': weeks,
-    }
-  elif delta < timedelta(days=365):
-    months = int(delta.days / 31)
-    msg = ngettext('%(months)s month ago',
-            '%(months)s months ago', months) % {
-      'months': months,
-    }
-  else:
-    years = int(delta.days / 365)
-    msg = ngettext('%(years)s year ago',
-            '%(years)s years ago', years) % {
-      'years': years,
-    }
-  return mark_safe(msg)
+    delta = datetime.now() - date
+
+    if delta < timedelta(seconds=60):
+        msg = _("Just now")
+    elif delta < timedelta(seconds=60 * 60):
+        minutes = delta.seconds / 60
+        msg = ngettext("%(minutes)s minute ago", "%(minutes)s min ago", minutes) % {
+            "minutes": minutes
+        }
+    elif delta < timedelta(days=1):
+        hours = int(delta.seconds / 60 / 60)
+        msg = ngettext("%(hours)s hour ago", "%(hours)s hours ago", hours) % {
+            "hours": hours
+        }
+    elif delta < timedelta(days=7):
+        msg = ngettext("%(days)s day ago", "%(days)s days ago", delta.days) % {
+            "days": delta.days
+        }
+    elif delta < timedelta(days=31):
+        weeks = int(delta.days / 7)
+        msg = ngettext("%(weeks)s week ago", "%(weeks)s weeks ago", weeks) % {
+            "weeks": weeks
+        }
+    elif delta < timedelta(days=365):
+        months = int(delta.days / 31)
+        msg = ngettext("%(months)s month ago", "%(months)s months ago", months) % {
+            "months": months
+        }
+    else:
+        years = int(delta.days / 365)
+        msg = ngettext("%(years)s year ago", "%(years)s years ago", years) % {
+            "years": years
+        }
+    return mark_safe(msg)
+
 
 @register.filter
 def cat(value, other_value):
     return "%s%s" % (value, other_value)
 
+
 @register.filter
 def urlize_twitter(text):
     return mark_safe(
-        re.sub(r'@([a-zA-Z0-9_]*)', r'@<a href="http://twitter.com/\1">\1</a>',
-            re.sub(r'#([a-zA-Z0-9_-]*)', r'<a href="http://twitter.com/#search?q=\1">#\1</a>', text)
+        re.sub(
+            r"@([a-zA-Z0-9_]*)",
+            r'@<a href="http://twitter.com/\1">\1</a>',
+            re.sub(
+                r"#([a-zA-Z0-9_-]*)",
+                r'<a href="http://twitter.com/#search?q=\1">#\1</a>',
+                text,
+            ),
         )
     )
